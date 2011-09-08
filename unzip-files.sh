f8ce086388934f57cbb05c36c84e34bd13bc0b5d
Decad3nce <abegovic23@gmail.com>
2011-09-07 22:09:59 -0500

    Update extract/unzip for proprietaries

--- unzip-files.sh
+++ unzip-files.sh
@@ -16,9 +16,6 @@
 
 DEVICE=tenderloin;
 MANUFACTURER=hp;
-DIRS="
-lib
-"
 
 if [ $1 ]; then
     ZIPFILE=$1
@@ -33,11 +30,59 @@ fi
 
 mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 
+DIRS="
+bin
+etc
+etc/firmware
+lib
+lib/hw
+lib/egl
+"
+
 for DIR in $DIRS; do
-	mkdir -p ../../../vendor/$MANUFACTURER/proprietary/$DIR
+	mkdir -p $PATHTOVENDOR/proprietary/$DIR
 done
 
 #SHARED OBJECT LIBRARIES
-unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/proprietary/system/lib/ \
+unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/proprietary/lib \
     system/lib/libcamera.so \
     system/lib/libaudioalsa.so \
+    system/lib/libaudcal.so \
+    system/lib/libdiag.so \
+    system/lib/libgemeni.so \
+    system/lib/libgsl.so \
+    system/lib/libmmpi.so \
+    system/lib/libOpenVG.so \
+    system/lib/libqdp.so \
+    system/lib/libqmi.so \
+    system/lib/libqmiservices.so 
+
+#EGL
+unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/proprietary/lib/egl \
+    system/lib/egl/libEGL_adreno200.so \
+    system/lib/egl/libGLESv1_CM_adreno200.so \
+    system/lib/egl/libGLESv2_adreno200.so \
+    system/lib/egl/libq3dtools_adreno200.so
+
+#HW
+unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/proprietary/lib/hw \
+    system/lib/hw/lights.msm8660.so \
+
+#BIN
+unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/poprietary/bin \
+    system/bin/dcvs \
+    system/bin/dcvsd \
+    system/bin/mpdecision \
+    system/bin/mpld \
+    system/bin/sensord \
+    system/bin/thermald \
+    system/bin/usbhub \
+    system/bin/usbhub_init \
+
+#Firmware
+unzip -j -o $ZIPFILE -d ../../../vendor/$MANUFACTURER/proprietary/etc/firmware \
+    system/etc/firmware/leia_pfp_470.fw \
+    system/etc/firmware/leia_pm4_470.fw \
+    system/etc/firmware/vidc_1080p.fw \
+    system/etc/firmware/yamato_pfp.fw \
+    system/etc/firmware/yamato_pm4.fw
