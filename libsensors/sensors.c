87ccdf3520ac6f3b2e7350a18393e4461db37ecf
Dalingrin <dalingrin@gmail.com>
2011-09-15 16:37:32 -0500

    Add libsensor
    Accelerometer is functional, magnetometer is not.
    Magnetometer libsensor should be done and
    the magnetometer functions at the kernel level.
    However, it seems apps that use the compass also
    use the GPS which results in a force close atm.

---
+++ libsensors/sensors.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <hardware/sensors.h>
+
+#include "nusensors.h"
+
+/*****************************************************************************/
+
+/*
+ * The SENSORS Module
+ */
+
+/*
+ * the AK8973 has a 8-bit ADC but the firmware seems to average 16 samples,
+ * or at least makes its calibration on 12-bits values. This increases the
+ * resolution by 4 bits.
+ */
+
+static const struct sensor_t sSensorList[] = {
+        { "LSM303DLH 3-axis Accelerometer",
+                "ST Microelectronics",
+                1, SENSORS_HANDLE_BASE+ID_A,
+                SENSOR_TYPE_ACCELEROMETER, 2.0f*GRAVITY_EARTH, (2.0f*GRAVITY_EARTH)/2048.0f, 0.5f, 10000000/*10ms*/, { } },
+        { "LSM303DLH Magnetometer",
+                "ST Microelectronics",
+                1, SENSORS_HANDLE_BASE+ID_M,
+                SENSOR_TYPE_MAGNETIC_FIELD, 1.0f, 1.0f, 0.5f, 10000000/*10ms*/, { } },
+
+};
+
+static int open_sensors(const struct hw_module_t* module, const char* name,
+        struct hw_device_t** device);
+
+static int sensors__get_sensors_list(struct sensors_module_t* module,
+        struct sensor_t const** list)
+{
+    *list = sSensorList;
+    return ARRAY_SIZE(sSensorList);
+}
+
+static struct hw_module_methods_t sensors_module_methods = {
+    .open = open_sensors
+};
+
+const struct sensors_module_t HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .version_major = 1,
+        .version_minor = 0,
+        .id = SENSORS_HARDWARE_MODULE_ID,
+        .name = "Hewlet Packard Sensors Module",
+        .author = "Erik Hardesty",
+        .methods = &sensors_module_methods,
+    },
+    .get_sensors_list = sensors__get_sensors_list
+};
+
+/*****************************************************************************/
+
+static int open_sensors(const struct hw_module_t* module, const char* name,
+        struct hw_device_t** device)
+{
+    return init_nusensors(module, device);
+}
