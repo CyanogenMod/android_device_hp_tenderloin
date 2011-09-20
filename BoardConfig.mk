cd87baa94fb05d2476a092da906418922fc62e8a
Dalingrin <dalingrin@gmail.com>
2011-09-19 21:18:43 -0500

    Disable cscan
    Requires http://review.cyanogenmod.com/#change,8043

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ WPA_SUPPLICANT_VERSION      := VER_0_6_X
 BOARD_WLAN_DEVICE           := ath6kl
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/ath6kl.ko"
 WIFI_DRIVER_MODULE_NAME     := "ath6kl"
+BOARD_WEXT_NO_COMBO_SCAN	:= true
 
 # Audio 
 BOARD_USES_GENERIC_AUDIO := false
