99e5e5e460c22c2e750300244131781d3a42ebdd
root <root@enterprise.(none)>
2011-09-23 02:23:22 -0400

    Added bcattach source

--- init.qcom.bt.sh
+++ init.qcom.bt.sh
@@ -26,89 +26,22 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-BLUETOOTH_SLEEP_PATH=/proc/bluetooth/sleep/proto
-LOG_TAG="qcom-bluetooth"
-LOG_NAME="${0}:"
-
-hciattach_pid=""
-
-loge ()
-{
-  /system/bin/log -t $LOG_TAG -p e "$LOG_NAME $@"
-}
-
-logi ()
-{
-  /system/bin/log -t $LOG_TAG -p i "$LOG_NAME $@"
-}
-
-failed ()
-{
-  loge "$1: exit code $2"
-  exit $2
-}
-
 start_hciattach ()
 {
-  /system/bin/hciattach -n $BTS_DEVICE $BTS_TYPE $BTS_BAUD &
+  /system/bin/hciattach -n /dev/ttyHS0 bcsp &
   hciattach_pid=$!
   logi "start_hciattach: pid = $hciattach_pid"
-  echo 1 > $BLUETOOTH_SLEEP_PATH
 }
 
 kill_hciattach ()
 {
-  echo 0 > $BLUETOOTH_SLEEP_PATH
   logi "kill_hciattach: pid = $hciattach_pid"
   ## careful not to kill zero or null!
   kill -TERM $hciattach_pid
   # this shell doesn't exit now -- wait returns for normal exit
 }
 
-# mimic hciattach options parsing -- maybe a waste of effort
-USAGE="hciattach [-n] [-p] [-b] [-t timeout] [-s initial_speed] <tty> <type | id> [speed] [flow|noflow] [bdaddr]"
-
-while getopts "blnpt:s:" f
-do
-  case $f in
-  b | l | n | p)  opt_flags="$opt_flags -$f" ;;
-  t)      timeout=$OPTARG;;
-  s)      initial_speed=$OPTARG;;
-  \?)     echo $USAGE; exit 1;;
-  esac
-done
-shift $(($OPTIND-1))
-
-# Note that "hci_qcomm_init -e" prints expressions to set the shell variables
-# BTS_DEVICE, BTS_TYPE, BTS_BAUD, and BTS_ADDRESS.
-
-#Selectively Disable sleep
-BOARD=`getprop ro.product.device`
-
-case $BOARD in
-  msm7627a ) IBS="-H";;
-  *) IBS="";;
-esac
-POWER_CLASS=`getprop qcom.bt.dev_power_class`
-
-case $POWER_CLASS in
-  1) PWR_CLASS="-p 0" ;
-     logi "Power Class: 1";;
-  2) PWR_CLASS="-p 1" ;
-     logi "Power Class: 2";;
-  3) PWR_CLASS="-p 2" ;
-     logi "Power Class: CUSTOM";;
-  *) PWR_CLASS="";
-     logi "Power Class: Ignored. Default(1) used (1-CLASS1/2-CLASS2/3-CUSTOM)";
-     logi "Power Class: To override, Before turning BT ON; setprop qcom.bt.dev_power_class <1 or 2 or 3>";;
-esac
-
-eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS $IBS && echo "exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
-
-case $exit_code_hci_qcomm_init in
-  0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
-  *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;;
-esac
+/system/bin/qcattach
 
 # init does SIGTERM on ctl.stop for service
 trap "kill_hciattach" TERM INT
