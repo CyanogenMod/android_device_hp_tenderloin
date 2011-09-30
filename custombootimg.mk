e86628e4066bcdd96d5481b34ea53b552d44c709
Diogo Ferreira <defer@cyanogenmod.com>
2011-09-30 12:17:47 +0100

    Revert "tenderloin: Remove dummy boot.img"
    This reverts commit a41344e4bdaa9fa159f2cf8204188c56229e8046.

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,13 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/cm-uMulti
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/erase_image $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Boot image: $@")
+	##FIXME# uMulti needs to be passed to the OTA package
+	$(hide) touch $(PRODUCT_OUT)/boot.img
 	$(hide) mkdir -p $(PRODUCT_OUT)/uMulti/ramdisk/
 	$(hide) cp -r $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/uMulti/ramdisk/
 	$(hide) find $(PRODUCT_OUT)/uMulti/ramdisk/ | cpio -R 0:0 -H newc -o --quiet | gzip -9c > $(PRODUCT_OUT)/uMulti/andr-init.img
 	$(hide) $(MKIMAGE) -A ARM -T RAMDisk -C none -n Image -d $(PRODUCT_OUT)/uMulti/andr-init.img $(PRODUCT_OUT)/uMulti/uRamdisk
-	$(hide) $(MKIMAGE) -A arm -T multi -C none -n 'test-multi-image' -d $(TARGET_PREBUILT_KERNEL):$(PRODUCT_OUT)/uMulti/uRamdisk $@
+	$(hide) $(MKIMAGE) -A arm -T multi -C none -n 'test-multi-image' -d $(TARGET_PREBUILT_KERNEL):$(PRODUCT_OUT)/uMulti/uRamdisk $(PRODUCT_OUT)/cm-uMulti
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/rec-uMulti
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
