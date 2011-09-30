d52a99d251fb989d0265d901afe4df4bb76a0b3e
Diogo Ferreira <defer@cyanogenmod.com>
2011-09-30 12:19:21 +0100

    tenderloin: remove uMulti inclusion "fixme"

--- custombootimg.mk
+++ custombootimg.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/erase_image $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Boot image: $@")
-	##FIXME# uMulti needs to be passed to the OTA package
 	$(hide) touch $(PRODUCT_OUT)/boot.img
 	$(hide) mkdir -p $(PRODUCT_OUT)/uMulti/ramdisk/
 	$(hide) cp -r $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/uMulti/ramdisk/
