33146421ba84cd9a969c13974c68c41229a6dfcd
Dalingrin <dalingrin@gmail.com>
2011-10-18 02:21:33 -0500

    Libaudio: add headphone detection

--- alsa_sound/alsa_default.cpp
+++ alsa_sound/alsa_default.cpp
@@ -23,6 +23,8 @@
 #include <sys/ioctl.h>
 #include <sys/time.h>
 #include <sound/asound.h>
+#include <linux/uinput.h>
+#include <pthread.h>
 
 #include "AudioHardwareALSA.h"
 #include <media/AudioRecord.h>
@@ -92,9 +94,14 @@ int nelements, idle_standalone_fd, idle_collapse_fd;
 static struct dsp_control {
     int fd;
     struct snd_ctl_elem_id *pcm_playback_id;
-	struct snd_ctl_elem_id *pcm_capture_id;
-	struct snd_ctl_elem_id *speaker_stereo_rx_id;
-	struct snd_ctl_elem_id *speaker_mono_tx_id;
+    struct snd_ctl_elem_id *pcm_capture_id;
+    struct snd_ctl_elem_id *speaker_stereo_rx_id;
+    struct snd_ctl_elem_id *speaker_mono_tx_id;
+    struct snd_ctl_elem_id *line1outn_id;
+    struct snd_ctl_elem_id *line1outp_id;
+    struct snd_ctl_elem_id *line2outn_id;
+    struct snd_ctl_elem_id *line2outp_id;
+    struct snd_ctl_elem_id *outvol_id;
 } dsp;
 
 int get_elem_list( int fd, struct snd_ctl_elem_list* list )
@@ -525,15 +532,48 @@ status_t setGlobalParams(alsa_handle_t *handle)
 
 // ----------------------------------------------------------------------------
 
+void* headphone_thread(void*)
+{
+    int fd = open("/dev/input/event5", O_RDONLY);
+    if (fd <= 0) {
+        LOGE("Error opening event file for headphone detection");
+        return 0;
+    }
+    struct input_event ev;
+
+    while (1)
+    {
+	    read(fd, &ev, sizeof(struct input_event));
+
+        if(ev.type == 5) {
+	        if(ev.value)
+            {
+                LOGI("Headphones enabled");
+                write_elem(dsp.fd,dsp.outvol_id,0x75,0x75,0);
+                write_elem(dsp.fd, dsp.line1outn_id, 0, 0, 0);
+                write_elem(dsp.fd, dsp.line1outp_id, 0, 0, 0);
+                write_elem(dsp.fd, dsp.line2outn_id, 0, 0, 0);
+                write_elem(dsp.fd, dsp.line2outp_id, 0, 0, 0);
+            } else {
+                LOGI("Headphones disabled");
+                write_elem(dsp.fd, dsp.line1outn_id, 1, 1, 1);
+                write_elem(dsp.fd, dsp.line1outp_id, 1, 1, 1);
+                write_elem(dsp.fd, dsp.line2outn_id, 1, 1, 1);
+                write_elem(dsp.fd, dsp.line2outp_id, 1, 1, 1);
+                write_elem(dsp.fd,dsp.outvol_id,0x3B,0x3B,0);
+            }
+        }
+    }
+    return 0;
+}
+
 static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
 {
+    pthread_t thread;
+
     struct snd_ctl_elem_list elem_list;
     struct snd_ctl_elem_id *line1mix_id;
-    struct snd_ctl_elem_id *line1outn_id;
-    struct snd_ctl_elem_id *line1outp_id;
     struct snd_ctl_elem_id *line2mix_id;
-    struct snd_ctl_elem_id *line2outn_id;
-    struct snd_ctl_elem_id *line2outp_id;
     struct snd_ctl_elem_id *leftdacmix_id;
     struct snd_ctl_elem_id *rightdacmix_id;
     struct snd_ctl_elem_id *aif2adc_id;
@@ -549,7 +589,6 @@ static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
     struct snd_ctl_elem_id *in1lzc_id;
     struct snd_ctl_elem_id *mixinl1_id;
     struct snd_ctl_elem_id *mixinlvol_id;
-    struct snd_ctl_elem_id *outvol_id;
     struct snd_ctl_elem_id *dac1aifl_id;
     struct snd_ctl_elem_id *dac1aifr_id;
     struct snd_ctl_elem_id *dac1_id;
@@ -582,11 +621,11 @@ static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
     dsp.speaker_stereo_rx_id = get_id("speaker_stereo_rx");
     dsp.speaker_mono_tx_id = get_id("speaker_mono_tx");
     line1mix_id = get_id("LINEOUT1 Mixer Output Switch");
-    line1outn_id = get_id("LINEOUT1N Switch");
-    line1outp_id = get_id("LINEOUT1P Switch");
+    dsp.line1outn_id = get_id("LINEOUT1N Switch");
+    dsp.line1outp_id = get_id("LINEOUT1P Switch");
     line2mix_id = get_id("LINEOUT2 Mixer Output Switch");
-    line2outn_id = get_id("LINEOUT2N Switch");
-    line2outp_id = get_id("LINEOUT2P Switch");
+    dsp.line2outn_id = get_id("LINEOUT2N Switch");
+    dsp.line2outp_id = get_id("LINEOUT2P Switch");
     leftdacmix_id = get_id("Left Output Mixer DAC Switch");
     rightdacmix_id = get_id("Right Output Mixer DAC Switch");
     aif2adc_id = get_id("AIF2ADC HPF Switch");
@@ -602,19 +641,19 @@ static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
     in1lzc_id = get_id("IN1L ZC Switch");
     mixinl1_id = get_id("MIXINL IN1L Switch");
     mixinlvol_id = get_id("MIXINL IN1L Volume");
-    outvol_id = get_id("Output Volume");
+    dsp.outvol_id = get_id("Output Volume");
     dac1aifl_id = get_id("DAC1L Mixer AIF1.1 Switch");
     dac1aifr_id = get_id("DAC1R Mixer AIF1.1 Switch");
     dac1_id = get_id("DAC1 Switch");
     dac2_id = get_id("DAC2 Switch");
 
     write_elem(dsp.fd,line1mix_id,1,0,0);
-    write_elem(dsp.fd,line1outn_id,1,0,0);
-    write_elem(dsp.fd,line1outp_id,1,0,0);
+    write_elem(dsp.fd,dsp.line1outn_id,1,0,0);
+    write_elem(dsp.fd,dsp.line1outp_id,1,0,0);
 
     write_elem(dsp.fd,line2mix_id,1,0,0);
-    write_elem(dsp.fd,line2outn_id,1,0,0);
-    write_elem(dsp.fd,line2outp_id,1,0,0);
+    write_elem(dsp.fd,dsp.line2outn_id,1,0,0);
+    write_elem(dsp.fd,dsp.line2outp_id,1,0,0);
 
     write_elem(dsp.fd,leftdacmix_id,1,0,0);
     write_elem(dsp.fd,rightdacmix_id,1,0,0);
@@ -633,7 +672,7 @@ static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
 
     write_elem(dsp.fd,mixinl1_id,1,0,0);
     write_elem(dsp.fd,mixinlvol_id,0,0,0);
-    write_elem(dsp.fd,outvol_id,0x3B,0x3B,0);
+    write_elem(dsp.fd,dsp.outvol_id,0x3B,0x3B,0);
     write_elem(dsp.fd,dac1aifl_id,1,0,0);
     write_elem(dsp.fd,dac1aifr_id,1,0,0);
 
@@ -661,6 +700,8 @@ static status_t s_init(alsa_device_t *module, ALSAHandleList &list)
     _defaultsIn.bufferSize = bufferSize;
 
     list.push_back(&_defaultsIn);
+
+    pthread_create (&thread, NULL, &headphone_thread, NULL);
 
     return NO_ERROR;
 }
