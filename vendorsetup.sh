ac0f84f2fc113e2eb687cd2fc465c6809462725a
Dalingrin <dalingrin@gmail.com>
2012-01-18 13:07:41 -0600

    remove old targets to avoid confusion

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,6 +19,3 @@
 #
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
-
-add_lunch_combo full_tenderloin-userdebug
-add_lunch_combo full_tenderloin-eng
