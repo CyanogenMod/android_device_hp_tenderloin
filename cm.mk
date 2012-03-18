329b90265de5608d6265a32bb9871ff82e540c91
eyeballer <eyeballer@teamw.in>
2012-03-18 01:20:52 -0400

    Set bootanimation.zip resolution
    Per: http://review.cyanogenmod.com/#change,13579

--- cm.mk
+++ cm.mk
@@ -1,6 +1,7 @@
 $(call inherit-product, device/hp/tenderloin/device_tenderloin.mk)
 
 PRODUCT_RELEASE_NAME := Touchpad
+TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
