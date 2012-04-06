5ed684b4d3549d68cd6cda9ff337e81319fe5d5a
eyeballer <eyeballer@teamw.in>
2012-04-05 23:05:32 -0400

    update fingerprint for 4.0.4
    Change-Id: Icb90c70ee54b625d22bed41fd4426e386a82be8f

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=IML74K BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.0.3/IML74K/223971:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.0.3 IML74K 223971 release-keys" BUILD_NUMBER=189904
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.0.4/IMM76D/223971:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.0.4 IMM76D 223971 release-keys"
 
 PRODUCT_NAME := cm_tenderloin
 PRODUCT_DEVICE := tenderloin
