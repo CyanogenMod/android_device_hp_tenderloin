0f13d49cdc312c14256aedf9c3929348dda316ab
James Sullins <jcsullins@gmail.com>
2012-07-06 09:47:00 -0500

    prebuilt: update wifi modules to CMTP-ATH6KL v4.4
    CMTP-ATH6KL v4.4 is based on compat-wireless-3.5-rc3-1-sn
    with tenderloin specific modifications. It also bumps max_power
    for better handling of weak signals.
    
    Note that v4.4 is the same as v4.3.2.
    
    Change-Id: I95ee5ac137208be37a3458ebe2a1a8350c7c2f12

--- device_tenderloin.mk
+++ device_tenderloin.mk
@@ -177,7 +177,9 @@ PRODUCT_COPY_FILES += \
     device/hp/tenderloin/prebuilt/wifi/ath.ko:system/lib/modules/ath.ko \
     device/hp/tenderloin/prebuilt/wifi/cfg80211.ko:system/lib/modules/cfg80211.ko \
     device/hp/tenderloin/prebuilt/wifi/mac80211.ko:system/lib/modules/mac80211.ko \
-    device/hp/tenderloin/prebuilt/wifi/compat.ko:system/lib/modules/compat.ko
+    device/hp/tenderloin/prebuilt/wifi/compat.ko:system/lib/modules/compat.ko \
+    device/hp/tenderloin/prebuilt/wifi/sch_codel.ko:system/lib/modules/sch_codel.ko
+    device/hp/tenderloin/prebuilt/wifi/sch_fq_codel.ko:system/lib/modules/sch_fw_codel.ko
 
 #Wifi Firmware
 # from kernel.org
