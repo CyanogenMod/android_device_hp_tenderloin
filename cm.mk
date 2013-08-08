13abb39a0215aa9111bfa8b5eb1365fae4a00646
milaq <micha.laqua@gmail.com>
2013-08-09 00:11:18 +0200

    update build fingerprint for mr2

--- cm.mk
+++ cm.mk
@@ -14,4 +14,4 @@ PRODUCT_BRAND := HP
 PRODUCT_MODEL := TouchPad
 PRODUCT_MANUFACTURER := HP
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.2.2 JDQ39 573038 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.3/JWR66V/737497:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.3 JWR66V  737497 release-keys"
