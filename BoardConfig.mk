2c9878c7eb2774e4699bcbaacc3c8f73e4384a45
milaq <micha.laqua@gmail.com>
2013-09-12 16:52:34 +0200

    we have a scorpion variant by now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a8
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := scorpion
 TARGET_CPU_SMP := true
 
 TARGET_NO_RADIOIMAGE := true
