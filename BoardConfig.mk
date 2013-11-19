2723be1999ae050736bfe004a4f4f59d65ea8f09
Micha LaQua <micha.laqua@gmail.com>
2013-11-19 14:36:40 +0100

    use generic a8 variant until scorpion is fixed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a8
-TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_VARIANT := cortex-a8
 TARGET_CPU_SMP := true
 
 TARGET_DISABLE_ARM_PIE := true
