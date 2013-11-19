41465359a745385c61c72f2a7b1765c16956259d
Micha LaQua <micha.laqua@gmail.com>
2013-11-19 15:24:13 +0100

    update fingerprint for kitkat

--- cm.mk
+++ cm.mk
@@ -14,4 +14,4 @@ PRODUCT_BRAND := HP
 PRODUCT_MODEL := TouchPad
 PRODUCT_MANUFACTURER := HP
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.3/JWR66V/737497:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.3 JWR66V  737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.4/KRT16M/737497:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.4 KRT16M 737497 release-keys"
